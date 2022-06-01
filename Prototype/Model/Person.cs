0dd01f4bb4163a29fc44d4482208519a4b47e0fe
Jose Filho <jose.filho@ao3tech.com>
2022-06-01 14:03:17 -0300

    Prototype

---
+++ Prototype/Model/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace Prototype.Model;
+
+public class Person
+{
+	public int Age;
+	public DateTime BirthDate;
+	public string? Name;
+	public IdInfo? IdInfo;
+
+	public Person ShallowCopy()
+	{
+		return (Person)MemberwiseClone();
+	}
+
+	public Person DeepCopy()
+	{
+		var clone = (Person)MemberwiseClone();
+		clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+		clone.Name = string.Copy(Name);
+		return clone;
+	}
+}
