f7c97cb3292fd67566cc488123b7fefe04d02179
Jose Filho <jose.filho@ao3tech.com>
2022-06-02 09:00:22 -0300

    Design Singleton

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignSingleton;
+
+public sealed class Singleton
+{
+	private Singleton() { }
+
+	private static Singleton? _instance;
+
+	public static Singleton GetInstance()
+	{
+		if (_instance == null)
+		{
+			_instance = new Singleton();
+		}
+		return _instance;
+	}
+}
